dba503c96e73763a9669760e5f1ccdafe755b27d
luozhu1998 <718366079@qq.com>
2023-09-28 12:32:19 +0800

    First Init

---
+++ Source/FirstProjectEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class FirstProjectEditorTarget : TargetRules
+{
+	public FirstProjectEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		ExtraModuleNames.Add("FirstProject");
+	}
+}
